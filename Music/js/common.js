9dbe783aaf64639f17112ba40fc496027bf2f3c5
Max <vovkak13@gmail.com>
2017-08-21 21:43:08 +0300

    Update common.js

--- Music/js/common.js
+++ Music/js/common.js
@@ -1,7 +1,14 @@
+$(document).ready(function() {
+
+
+createSong();
+createAudio();
+
 function createSong(){
 	let container = document.createElement('div');
 	let playButton = document.createElement('button');
 	playButton.innerHTML = 'Play';
+	playButton.id = '178538';
 	let name = document.createElement('span');
 	name.innerHTML = 'Song Name and artist';
 
@@ -12,12 +19,14 @@ function createSong(){
 }
 
 function createAudio(){
+
+	console.log('lalaks');
 	let audio = document.createElement('audio');
 	audio.setAttribute('controls', 'controls');
 	audio.id = 'audio';
 
 	let source = document.createElement('source');
-	source.setAttribute('src', 'https://strm.yandex.ru/get/Hxtu01NhbHRlZF9fE9iM9Uh8QBlQ_N14GmAjvnIOfLgKnuKH1oCFnjzSZaBTCu1iM9ac0cBa9tB1wnkVJBll07BGXHwO9JPg5AcQDvPgZ8qDQtB0jFmoSxRs_QVZLP12tgUwhk4N_dPCD6E2C3p3xIyvB2ZFn9LzR2Fw-9GFbIq9Myuj_O02q3mQ1wunpR7XW8XJBrUGLTmdzrxock5VN5nNd_2YaH15b4Hh2X3QO61UIdqJDaJEGbZBkiq9xPy-cGCuTpnf5nBiSR-V28X9zdXwWSZ2vcidVuF9MYlItdvwB7BCkq6tqsgEZDPzithIXsgJsvLUM3ZZH23U44xGEEMEzSlr9ux6AP4yY-Jnr87ncqjCGEC6kFI5vh5dB-LIGZ_FmK8iVtq7LIDxopbgP90fDIjI72aH2YuaRH77byWyTlJVnO47BQLWR_or_K84yjjtSKAmaVnCi3P8OgLE795Iwqf-Fp0Xp8K84FGOwhL3TgVZGsv5jpF60MmoMnRsiPUv1sRq4hNSFq0cM_6sd46xInBihRqyCPV60D_CipVOVzsewFpq3CfRw_e_cFRlH7FOBHUCKlTd32Hleqz9UdPgzp9P6Jel2mNk-InVmGprySY5z-R6cYWura7IkSx4n8iruG9FYxsTq3ljiwWy7ERwsYHFfPDW68EPbVelZIB5lsQMLCanEe4i7yiTOViRsyBlv_IRan141Wn3ZUiZXfj__j2gMq5aJd8FzO-OLvKngXPnRiudEI9zO-xb3L-vKOObf2dXC8j8sJmEl9XykYtH-W_eFSVXUt_46G6dsGBcIIZPo4W2oTCHrpT0VwqmuQqV_2ZUMs5D63uYxrv5LCeoViViv6ISsb-KpkVZ2p23UMlY_AQjNA7DktlTsley0fjWDegVliaxT4cK0e8KCXNjrW_s3fit4Zc7uqb5QOtugiFYrH14kLvwuriUlC9li3Aa_ywRtzsL0rAyU_3fqfZe5XQuePANQtwhzCiN9NrzVAlv?ts=5988b10f&sign=fa093fc348b95f1ff58b0773e221177f');
+	source.setAttribute('src', '');
 	source.setAttribute('type', 'audio/mpeg');
 	source.id = 'audioSource';
 
@@ -26,17 +35,60 @@ function createAudio(){
 	document.body.appendChild(audio);
 }
 
-createSong();
-createAudio();
+async function getTrackInfo(trackId, albumInfo, trackNumber){
+	const baseUrl = 'https://music.yandex.ru';
+	const options = {
+        headers: {
+            'X-Retpath-Y': encodeURIComponent('https://music.yandex.ru/')
+        },
+        redirect: 'error',
+        credentials: 'include'
+    };
 
-$('button').on('click', function(){
-	console.log('lalka');
+    const trackInfoUrl = baseUrl + '/api/v2.1/handlers/track/' + trackId + '/track/download/m?hq=1';
 
-	var audio = document.getElementById('audio');
-	var source = document.getElementById('audioSource');
-	source.src = 'https://strm.yandex.ru/get/HcQqm1NhbHRlZF9fjybtVQ33B7f2gnxtR6TjeTX_OZediwl-U_rjr02hN-8bYIOdOKQyK6WztISYQSO6GCRhKFClN6n6AwMAIy-9Ki17lKXFjF99Uv2DuaCuL0GI2Y5Lozyw00S0lvO8xJWVAs-ZqkBsEEPMMXOEhXLL4w7RU6KyER5OQfZhCKf4ThTOc-pu9uUUZTL0W2ucfVMAbjXw7QBRRQA4kHA-UOW_SXZ2xS5CfxnjD4oDsH669wk_4URSvBhFMgK-XZoJH4khTgFmmgGe5E_svVxFoq5XreHbHSLFLDXtftNZxuEqKgPeECYIDEUiGW7n_6jgPMWRKSOlDk-_dQfxLjvzdH9J2m01JewmC021TPzZjo11IKNEi4gOMpCm3SJ63K5MDirZSV35eky-_0tgGtMtyaInwcWG-Yamx4y3zt3dpAZAd_-Z_jiETiuZ1kPZl2MdTSDhoMDwYHkzlYX_1l60OCyFMXgFcPoDEMf8eFZopqJnMQB8rkqSaViAzWRVXp5ogseE3uCAIwYoubvk2YUQu_tRMv2E_39KcFMQjvwtyqTGL-_vIcdiW77L6OmJGqb37flNEcsgtJc7NjOKkmuxXD3i1JVnt-z4GculXXF_Vu9iJFEPe9Pfhf3gS05l9uHyPYu4aw9K7n_K8nOO516eXu3NbKE9ZqkYn-3-PTMjo_7c4KNyd2id1cLOyz9O8H5sCXMv86qoTT22uUD3VmoeUsI5lOEyeCIh8QX2s6jSfOxcZK7LTT0nS8k-TLrbjNvy8jZse1xrkDemTkBW8PA5tsPfK16jYGdrJJ4FvrPZZ6PR9fnAdfiyvEq46Ts9koWh6bajeftId0hWIBgd5jENvxlYBq5K4xANP0vhgbK6Zg0UgLKnOkHOyNMh_qR0wSTvDba79BZ-dTqdNCvoP-emU8wuTtRhME1qEaVaAGJ_YoIQ9GWpk6mzYrJDoJR_3j0Zx8ACiqS2vbSsADkGVLqDCLosJyKygV9YYyKr?ts=5988b50e&sign=97641dd9ae88c0e7e779d84b178f0ea1';
+    const trackSrc = await fetch(trackInfoUrl, options).then(function(resp){return resp.json()});
+    const downloadInfo = await fetch(`${trackSrc.src}&format=json`).then(function(resp){return resp.json()});
+
+    const salt = 'XGRlBW9FXlekgbPrRHuSiA';
+    const hash = md5(salt + downloadInfo.path.substr(1) + downloadInfo.s);
+
+    let trackInfoJSON = await fetch(`${baseUrl}/handlers/track.jsx?track=${trackId}`, options).then(function(resp){return resp.json()});
+
+    let trackInfo = {
+        url: `https://${downloadInfo.host}/get-mp3/${hash}/${downloadInfo.ts + downloadInfo.path}`,
+        codec: trackSrc.codec,
+        artist: trackInfoJSON.track.artists[0].name,
+        title: trackInfoJSON.track.title,
+        genre: trackInfoJSON.track.albums[0].genre,
+        lyrics: trackInfoJSON.lyric[0].fullLyrics,
+        label: trackInfoJSON.track.albums[0].labels[0].name,
+        cover: 'https://' + trackInfoJSON.track.coverUri.split('%%')[0] + 'm1000x1000',
+        album: trackInfoJSON.track.albums[0].title,
+        albumArtist: trackInfoJSON.track.albums[0].artists[0].name,
+        year: 20
+    };
+
+    if('lyrics' in trackInfoJSON)
+        trackInfo.lyrics = trackInfoJSON.lyric[0].fullLyrics;
+    if (trackNumber)
+        trackInfo.number = trackNumber;
+
+    return trackInfo.url;
+};
+
+
+$('button').on('click', async function(){
+	console.log('click');
+
+	let audio = document.getElementById('audio');
+	let source = document.getElementById('audioSource');
+	source.src = await getTrackInfo(this.id);
+	console.log(this.id, source.src);
 
 	audio.load();
 	audio.play();
+
+});
 
 });
